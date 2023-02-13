22ddc54ed8b100f17e98a48b3cf91412864a63d3
pavankumar-mn <88427143+pavankumar-mn@users.noreply.github.com>
2023-02-13 20:34:20 +0530

    Add project files.

---
+++ WebApplication1/MiddlewareTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MiddlewareTest
+    {
+        private readonly RequestDelegate _next;
+
+        public MiddlewareTest(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            Console.WriteLine("Hello Dear Readers!");
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareTestExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareTest(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareTest>();
+        }
+    }
+}
